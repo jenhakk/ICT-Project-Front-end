d40f1b8651873298c538882395d78a1bc66cd707
Jenna Hakkarainen <75015030+jenhakk@users.noreply.github.com>
2022-11-18 15:26:38 +0200

    Testing JSON Forms

--- speech-recognition/src/App.js
+++ speech-recognition/src/App.js
@@ -1,9 +1,45 @@
 import './App.css';
+import { person } from '@jsonforms/examples';
+import {
+  materialRenderers,
+  materialCells,
+} from '@jsonforms/material-renderers';
+import React, { useState } from 'react';
+import { JsonForms } from '@jsonforms/react';
 
 function App() {
+
+  const schema = person.schema;
+  const uischema = person.uischema;
+  const initialData = person.data;
+
+  const [data, setData] = useState(initialData);
+
+
   return (
     <div className="App">
-      <p>Testing</p>
+    <div>
+      <h1>Valitse oikea riskinarviopuu</h1>
+      <JsonForms
+        schema={schema}
+        uischema={uischema}
+        data={data}
+        renderers={materialRenderers}
+        cells={materialCells}
+        onChange={({ data, errors }) => setData(data)}
+      />
+
+    </div>
+
+    <div>
+      <li style={{listStyleType:"none"}}>
+        <ul>Kaatunut puu</ul>
+        <ul>Myymälävarkaus</ul>
+      </li>
+    </div>
+   
+
+
     </div>
   );
 }
