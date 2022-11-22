8e41b84e4119ace020c60103a8787231bcec7334
Jenna Hakkarainen <75015030+jenhakk@users.noreply.github.com>
2022-11-22 16:29:33 +0200

    Creating forms and fetch

--- speech-recognition/src/App.js
+++ speech-recognition/src/App.js
@@ -1,23 +1,78 @@
 import './App.css';
-import FallentreeForm from './pages/FallentreeForm';
-
+import React, { useEffect, useState } from 'react';
+import FallentreeForm from './pages/FallentreeForm'
+import ShopliftingForm from './pages/ShopliftingForm';
 
 function App() {
 
+  const [data, setData] = useState('');
+  const LOCAL_ADDRESS = 'http://127.0.0.1:8081';
+  const SERVICE_ADDRESS = LOCAL_ADDRESS;
+  const [isLoading, setLoading] = useState(true);
+  const [chosenIncident, setIncident] = useState("");
+  const [count, setCount] = useState(0);
+
+
+
+  useEffect(() => {
+   const interval = setInterval(() => {
+      fetchData();   
+      console.log("count ",count);
+
+    }, 5000); 
+    return () => {
+      clearInterval(interval);
+    }
+    }, []);
+
+
+  useEffect(() => {
+    console.log("chosenEffect ", chosenIncident);
+    window.localStorage.setItem('chosenIncident', chosenIncident);
+     console.log("local ", window.localStorage.getItem('chosenIncident'));
+  }, [chosenIncident]);
+
+  useEffect(() => {
+    console.log("chosenEffect2 ", chosenIncident);
+    const data = window.localStorage.getItem('chosenIncident');
+     console.log("data ", data);
+    setIncident(data);
+  }, []);  
+
+  const chooseIncident = (incidentSelected) => {
+    setIncident(incidentSelected);
+  } 
+
+  const fetchData = async () => {
+    try {
+      let response = await fetch(
+        SERVICE_ADDRESS + '/rest/speechservice/getdata',
+      );
+      let json = await response.text();
+      setData(json);
+      console.log("onko tämä json     ",json);
+     
+  
+    } catch (error) {
+      console.log("onko tämä error", error);
+    }
+  };
+
   return (
     <div className="App">
-
-      <FallentreeForm />
     <div>
-      <h1>Valitse oikea riskinarviopuu testing</h1>
-      <li style={{listStyleType:"none"}}>
-        <ul>Kaatunut puu</ul>
-        <ul>Myymälävarkaus</ul>
-      </li>
+      <h1>Valitse oikea riskinarviopuu</h1>
+  
+        <button onClick={(e) => chooseIncident(e.target.value)} value="puu">Kaatunut puu</button>
+        <button onClick={(e) => chooseIncident(e.target.value)} value="myymala">Myymälävarkaus</button>
+      
+
+      <p>Tämä tulee backendistä: {data}</p>
+
 
     </div>
-    <div>
-     
+    <div>{chosenIncident=== "puu" ? <FallentreeForm /> : chosenIncident === "myymala" ? <ShopliftingForm /> : "" }
+
     </div>
 
     
--- speech-recognition/src/pages/FallentreeForm.js
+++ speech-recognition/src/pages/FallentreeForm.js
@@ -1,63 +1,93 @@
-import React from 'react';
-import Form from '../components/Form';
+import React, {useState} from 'react';
+import { useForm } from 'react-hook-form';
+import useFormPersist from 'react-hook-form-persist';
 
-function FallentreeForm(props) {
 
-    let template = {
-        title: 'Kaatunut puu',
-        fields: [
-            {
-                title: 'Mikä on puiden lukumäärä?',
-                type: 'radio',
-                name:'puidenlkm',
-               
-                
-            },
-            {
-                title: 'Mikä on puiden lukumäärä?',
-                type: 'radio',
-                register: 'puidenlkm',
-                value: '2'
-                
-            },
-            {
-                title: 'Email',
-                type: 'email',
-                name: 'email'
-            },
-            {
-                title: 'Include Portfolio',
-                type: 'checkbox',
-                name: 'include_portfolio'
-            },
-            {
-                title: 'Portfolio Link',
-                type: 'url',
-                name: 'portfolio_link',
-                dynamic: {
-                    field: 'include_portfolio',
-                    value: true
-                }
-            }
-        ]
-    }
+function FallentreeForm() {
 
-    return (
-        <Form
-            template={template}
-            watchFields={['include_portfolio']}
-            onSubmit={onSubmit}
-        
-        />
-    );
+    
+    const [formData, saveFormData] = useState();
+    const { register, handleSubmit, watch, setValue} = useForm();
+    
+    const onSubmit = data => {saveFormData(data)};
+    console.log("mikä tämä on ",formData);
+
+    useFormPersist("storageKey", {
+      watch, 
+      setValue,
+      storage: window.localStorage, // default window.sessionStorage
+    });
+    
+    const watchEstaakoLiikennetta = watch("estaakoLiikennetta");
+    const watchIhmisvahinkoa = watch("ihmisvahinko");
+      
+      return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+            <label>Mikä on puiden lukumäärä?</label>
+                < br/>
+            <label htmlFor='puidenlkm'><input {...register("puidenlkm")} type="radio" value="1"/>1</label>
+            <label><input {...register("puidenlkm")} type="radio" value="2"/>2</label>
+            <label><input {...register("puidenlkm")} type="radio" value="3"/>3</label>
+            <label><input {...register("puidenlkm")} type="radio" value="monta"/>Monta</label>
+                <br />
+                <br />
+    
+          <label>Estääkö tapahtuma liikennettä?</label>
+                < br/>
+          <label><input {...register("estaakoLiikennetta", { required: true })} type="radio" value="kylla" /> Kyllä</label>
+          <label><input {...register("estaakoLiikennetta", { required: true })} type="radio" value="ei" /> Ei</label>
+
+                <br />
+                <br />
+
+          {watchEstaakoLiikennetta === "kylla" && (
+            <div>
+
+                <label>Mitä liikennettä?</label>
+                    < br/>
+                <label><input {...register("mitaLiikennetta")} type="radio" value="moottoritie" />Moottoritie</label>
+                <label><input {...register("mitaLiikennetta")} type="radio" value="maantie" />Maantie</label>
+                <label><input {...register("mitaLiikennetta")} type="radio" value="taajama" />Taajama</label>
+                <label><input {...register("mitaLiikennetta")} type="radio" value="junaraiteet" />Junaraiteet</label>
+                <label><input {...register("mitaLiikennetta")} type="radio" value="pihatie" />Pihatie</label>
+                <label><input {...register("mitaLiikennetta")} type="radio" value="kevyt" />Kevyen liikenteen väylä</label>
+                    <br />
+                    <br />
+              </div>
+          )}
+          
+          <label>Onko ihmisvahinkoa?</label>
+              < br/>
+          <label><input {...register("ihmisvahinko", { required: true })} type="radio" value="kylla" />Kyllä</label>
+          <label><input {...register("ihmisvahinko", { required: true })} type="radio" value=" ei" />Ei</label>
+              <br />
+              <br />
+
+          {watchIhmisvahinkoa === "kylla" && (
+          <div>
+              <label>Millaista vahinkoa?</label>
+                  < br/>
+              <label><input {...register("mitaVahinkoa")} type="radio" value="lieva" />Lievästi loukkaantunut</label>
+              <label><input {...register("mitaVahinkoa")} type="radio" value=" vakava" />Vakavasti loukkaantunut</label>
+              <label><input {...register("mitaVahinkoa")} type="radio" value=" eloton" />Eloton</label>
+            
+                  <br />
+                  <br />
+              <label>Monta ihmistä on vahingoittunut?</label>
+                  < br/>
+              <label><input {...register("ihmistenlkm")} type="radio" value="1" />1</label>
+              <label><input {...register("ihmistenlkm")} type="radio" value=" 2" />2</label>
+              <label><input {...register("ihmistenlkm")} type="radio" value=" 3" />3</label>
+              <label><input {...register("ihmistenlkm")} type="radio" value=" useita" />Useita</label>
+                  <br />
+                  <br />
+          </div>
+          )}
+          <input type="submit" />
+        </form>
+      );
+    
 }
-
-
-function onSubmit(values) {
-    console.log(values);
-}
-
-
-
 
 export default FallentreeForm;
